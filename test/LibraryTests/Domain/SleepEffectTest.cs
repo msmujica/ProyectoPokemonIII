7c1705015a12b342744748784e6f180be45b8048
Martin <mujica0106@gmail.com>
2024-11-20 14:16:05 -0300

    Tests efectos modificados

---
+++ test/LibraryTests/Domain/SleepEffectTest.cs
@@ -0,0 +1,29 @@
+using Library;
+using NUnit.Framework;
+
+namespace LibraryTests.Domain;
+
+[TestFixture]
+[TestOf(typeof(SleepEffect))]
+public class SleepEffectTest
+{
+
+    [Test]
+    public void TestStartEffect()
+    {
+        Pokemon Pikachu = new Pokemon("Pikachu", 100, new List<string> { "Impactrueno", "Rayo", "Trueno"}, "Eléctrico");
+        SleepEffect sleep = new SleepEffect();
+        string result = sleep.StartEffect(Pikachu);
+        Assert.That(result, !Is.Null);
+    }
+
+    [Test]
+    public void TestProcessEffect()
+    {
+        Pokemon Pikachus = new Pokemon("Pikachu", 100, new List<string> { "Impactrueno", "Rayo", "Trueno"}, "Eléctrico");
+        SleepEffect sleep = new SleepEffect();
+        sleep.StartEffect(Pikachus);
+        string result = sleep.ProcessEffect(Pikachus);
+        Assert.That(result, !Is.Null);
+    }
+}
